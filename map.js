ba08f06d72ac514c6053278f164bb6b79a3727af
Shafee Rushdan <srushdan@mbp-010668.myfiosgateway.com>
2022-01-06 05:55:11 -0800

    roughDraft

--- map.js
+++ map.js
@@ -1,18 +1,33 @@
+let playerArr = [["P",0,0,0],[0,"P",0,0],[0,0,"P",0],[0,0,0,"P"]]
+let monstersArr = [["M",0,0,0],[0,"M",0,0],["M",0,0,0],[0,"M",0,"M"]]
 class Map {
-    constructor (){
-       
-        this.board = []
-        
-    }
-
-    genNewBoard(){
-       
-     return   this.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    }
-
-    printBoard(){
-      return  this.board.forEach(x => console.log(x))
-    }
+  constructor (){
+    
+    this.board = []
+    
+  }
+  
+  genNewBoard(){
+    return   this.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    
+  }
+  
+  printBoard(){
+    return  this.board.forEach(x => console.log(x))
+  }
+  
 }
+
+const randomArrP = Math.floor(Math.random() * playerArr.length) 
+const randomPlayer = playerArr[randomArrP] 
+
+const randomArrM = Math.floor(Math.random() * monstersArr.length) 
+const randomMonster = monstersArr[randomArrM] 
+
+  console.log(randomPlayer)
+  console.log(randomMonster)
+  console.log(randomMonster)
+  console.log(randomMonster)
+
 
 module.exports = Map 
